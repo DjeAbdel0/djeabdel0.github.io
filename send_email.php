40bf160284d086aea892009dd416f84f028872c0
Abdel <djeabdel0@gmail.com>
2024-10-10 20:51:09 -0400

    test upload vid

---
+++ send_email.php
@@ -0,0 +1,22 @@
+<?php
+// https://formspree.io/
+// mdp: ajouter @ à la fin
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    $to = "djeabdel0@gmail.com";
+    $subject = "Nouveau message de contact";
+    $body = "Nom: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message envoyé avec succès.";
+    } else {
+        echo "Échec de l'envoi du message.";
+    }
+}
+?>
+
